be92b3cc7cfdc675f6a96fa3dadd30df565f4748
codeNchrome <86122541+codeNchrome@users.noreply.github.com>
2024-11-01 07:30:09 -0400

    Adding comments

--- Board.java
+++ Board.java
@@ -1,1 +1,13 @@
 //Board file
+
+//Whats in the box
+//5 ships
+//	Carrier has 5 pegs
+
+//2 boards
+	//rows and columns
+	//10x10
+	//letters down the side
+	//numbers across top
+//2 colored sets of pegs
+
